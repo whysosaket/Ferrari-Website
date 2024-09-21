df5cad0705eff212c29939efef40b0cea5c97eee
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 00:15:41 +0530

    Added Vector

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,16 +1,16 @@
 const Info = () => {
   return (
-    <div className="w-2/5 px-10 -mt-40">
-      <h1 className="text-4xl font-semibold">
+    <div className="w-2/5 px-10 -mt-40 flex flex-col">
+      <h1 className="text-4xl font-semibold z-10">
         Innovating the Future of Electric Vehicles
       </h1>
       <div className="my-2 flex gap-8">
-        <p>
+        <p className="z-10">
           Ferrari is innovating the future of electric vehicles. By harnessing
           advanced battery systems, lightweight materials, and precision
           aerodynamics.
         </p>
-        <p>
+        <p className="z-10">
           {" "}
           Ferrari is shaping a new era of electrification, without compromising
           the passion and legacy of its iconic brand.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import Car from "../assets/car1.png";
 import Info from "../components/Info";
 import Stats from "../components/Stats";
+import Background from "../assets/background.svg";
 
 const Home = () => {
   return (
-    <div className="bg-ternary rounded-3xl my-6 px-6 py-4">
-        <h1 className="font-anton text-[21rem] leading-none text-center">EXCLUSIVE</h1>
+    <div className="bg-ternary rounded-3xl my-6 px-6 py-4 overflow-hidden">
+        <img src={Background} className="w-full absolute top-0 -left-24 -scale-x-150 -rotate-[30deg]" />
+        <div className="flex justify-center">
+          <h1 className="font-anton flex z-10 text-[21rem] leading-none text-center">EXCLUSIVE</h1>
+        </div>
+        
         <div className="flex -mt-28">
-            <div className="w-1/3 mt-28 pl-10">
+            <div className="w-1/3 mt-28 pl-10 flex flex-col z-10">
                 <Stats />
             </div> 
-            <img className="w-2/3" src={Car} />
+            <img className="w-2/3 z-10" src={Car} />
         </div>
         <Info />
     </div>
