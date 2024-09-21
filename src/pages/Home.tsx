e9e73326cf31de3e24c55c2ca47effa011b0a2fb
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 00:22:25 +0530

    Fixed Background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,21 +5,29 @@ import Background from "../assets/background.svg";
 
 const Home = () => {
   return (
-    <div className="bg-ternary rounded-3xl my-6 px-6 py-4 overflow-hidden">
-        <img src={Background} className="w-full absolute top-0 -left-24 -scale-x-150 -rotate-[30deg]" />
-        <div className="flex justify-center">
-          <h1 className="font-anton flex z-10 text-[21rem] leading-none text-center">EXCLUSIVE</h1>
+    <div className="relative bg-ternary rounded-3xl my-6 px-6 py-4 overflow-hidden">
+      {/* Background image */}
+      <img src={Background} className="w-full absolute -top-16 -left-24 -scale-x-150 -rotate-[15deg]" />
+
+      {/* Exclusive text */}
+      <div className="flex justify-center">
+        <h1 className="font-anton flex z-10 text-[21rem] leading-none text-center">
+          EXCLUSIVE
+        </h1>
+      </div>
+
+      {/* Stats and car image */}
+      <div className="flex -mt-28 relative z-10">
+        <div className="w-1/3 mt-28 pl-10">
+          <Stats />
         </div>
-        
-        <div className="flex -mt-28">
-            <div className="w-1/3 mt-28 pl-10 flex flex-col z-10">
-                <Stats />
-            </div> 
-            <img className="w-2/3 z-10" src={Car} />
-        </div>
-        <Info />
+        <img className="w-2/3" src={Car} alt="Car" />
+      </div>
+
+      {/* Info section */}
+      <Info />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
