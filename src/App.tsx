2ad448409dae82875ad5c019878c3a611ffd037b
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 01:58:16 +0530

    Added Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react"
 import BottomBar from "./components/BottomBar"
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home"
+import Loading from "./components/Loading"
 
 function App() {
-
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false);
+    },1500)
+  }, [])
   return (
-    <div className="px-6 py-4 font-inter">
+      loading?<Loading />:<div className="px-6 py-4 font-inter">
       <Navbar />
       <Home />
       <BottomBar />
